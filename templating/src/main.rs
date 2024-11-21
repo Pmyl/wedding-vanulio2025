6a7aae05cef6bec609742080606681c402853300
Giulio Caprino <giulio.caprino@dataglide.co>
2024-11-21 19:48:36 +0000

    push index

--- templating/src/main.rs
+++ templating/src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::fs;
-use std::fs::File;
 use std::io::Write;
 use tera::{Context, Tera};
 
@@ -11,16 +10,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         include_str!("../../templating/src/templates/translations.json"),
     )?;
 
+    println!("English language");
     let translations_json_value = translations_json.remove("en").unwrap();
     let context = Context::from_value(translations_json_value)?;
     let rendered = tera.render("index.html", &context)?;
-    File::create("public/index.html")?.write_all(rendered.as_bytes())?;
+    std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open("public/index.html")?
+        .write_all(rendered.as_bytes())?;
 
+    println!("Italian language");
     let translations_json_value = translations_json.remove("it").unwrap();
     let context = Context::from_value(translations_json_value)?;
     let rendered = tera.render("index.html", &context)?;
-    fs::create_dir("public/it")?;
-    File::create("public/it/index.html")?.write_all(rendered.as_bytes())?;
+    fs::create_dir("public/it").ok();
+    std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open("public/it/index.html")?
+        .write_all(rendered.as_bytes())?;
 
     Ok(())
 }
